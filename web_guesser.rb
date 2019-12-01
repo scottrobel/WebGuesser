690cfebe0d5bf2be0c3e381452a78b89213435f3
Scott <robelrain@gmail.com>
2019-12-01 00:59:51 -0500

    I4 extensions

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,25 +1,62 @@
 # frozen_string_literal: true
 
 require 'sinatra'
-require 'sinatra/reloader'
+require 'sinatra/reloader' if development?
 
-SECRET_NUMBER = rand(101)
-TOO_HIGH = SECRET_NUMBER + 5
-TOO_LOW = SECRET_NUMBER - 5
+$secret_number = rand(101)
+$too_high = $secret_number + 5
+$too_low = $secret_number - 5
+$guesses_left = 5
+$guesses = []
+$guess_messege_pairs = {}
+
 get '/' do
+  reset_guesses
   guess = params["guess"]
-  messege = check_guess(guess)
-  erb :index, locals: { messege: messege }
+  cheat_toggle = params["cheat"]
+  messege = "Please enter a valid number between 0 and 100."
+  erb :index, locals: { messege: messege, guesses_left: $guesses_left, cheat_mode: cheat_toggle, secret_number: $secret_number, guess_messege_pairs: $guess_messege_pairs }
 end
+
+post '/' do
+  guess = params["guess"]
+  cheat_toggle = params["cheat"]
+  messege = check_guess(guess)
+  $guess_messege_pairs[guess.match(/(\d+)/)[1]] = messege if messege.match?(/(High|Low)/)
+  erb :index, locals: { messege: messege, guesses_left: $guesses_left, cheat_mode: cheat_toggle, secret_number: $secret_number, guess_messege_pairs: $guess_messege_pairs }
+end
+
 def check_guess(guess)
-  guess = guess.match(/(\d+)/)
-  if !guess
-    "Please enter a valid number"
-  elsif guess[1].to_i > SECRET_NUMBER
-    guess[1].to_i > TOO_HIGH ? "Guess way too High" : "Guess too High"
-  elsif guess[1].to_i < SECRET_NUMBER
-    guess[1].to_i < TOO_LOW ? "Guess way too Low" : "Guess too Low"
-  elsif guess[1].to_i == SECRET_NUMBER
-    "Guess Correct"
+  guess = guess.match(/(\d+)/) unless guess.nil?
+  return "Please enter a valid number" if guess.nil?
+  guess = guess[1]
+  return "You already guessed that number" if $guesses.include? guess
+
+  $guesses << guess
+  $guesses_left -= 1
+  valid_guess(guess)
+end
+
+def valid_guess(guess)
+  if guess.to_i == $secret_number
+    reset_guesses
+    "Guess Correct! secret number has been reset"
+  elsif $guesses_left == 0
+    number = $secret_number
+    reset_guesses
+    "Out of guesses number has been reset.\nThe number was #{number}"
+  elsif guess.to_i > $secret_number
+    guess.to_i > $too_high ? "Guess way too High" : "Guess too High"
+  elsif guess.to_i < $secret_number
+    guess.to_i < $too_low ? "Guess way too Low" : "Guess too Low"
   end
+end
+
+def reset_guesses
+  $guesses_left = 5
+  $secret_number = rand(101)
+  $too_high = $secret_number + 5
+  $too_low = $secret_number - 5
+  $guesses = []
+  $guess_messege_pairs = {}
 end
